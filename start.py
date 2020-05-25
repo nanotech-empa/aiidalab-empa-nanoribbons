b05f03f45ead2bbbeb4b200f998a3d2d0cdac73f
Carlo Pignedoli <c.pignedoli@gmail.com>
2020-05-25 15:12:03 +0000

    start page cleaning

--- start.py
+++ start.py
@@ -5,23 +5,14 @@ def get_start_widget(appbase, jupbase):
     template = """
     <table>
     <tr>
-        <th style="text-align:center">Structures</th>
-        <th style="width:70px" rowspan=2></th>
-        <th style="text-align:center">Nanoribbons</th>
-        
-    <tr>
-    <td valign="top"><ul>
-    <li><a href="{appbase}/rescale_structure.ipynb" target="_blank">Scale structures</a>
-    <li><a href="{appbase}/construct_cell.ipynb" target="_blank">Construct cell</a>
-    <li><a href="{appbase}/edit_structure.ipynb" target="_blank">Assign spin, remove atoms</a>
-    </ul></td>
     
     <td valign="top"><ul>
     <li><a href="{appbase}/submit.ipynb" target="_blank">Submit calculation</a>
     <li><a href="{appbase}/search.ipynb" target="_blank">Search database</a>
     </ul></td>
     
-    </tr></table>
+    </tr>
+    </table>
 """
     
     html = template.format(appbase=appbase, jupbase=jupbase)
