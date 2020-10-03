0305d96701732dfa8ec7acb20ec6361cb811ccfa
Kristjan Eimre <kristjaneimre@gmail.com>
2020-10-03 17:08:08 +0000

    SSSP_modified automatic check & install in submit notebook

--- setup_sssp_modified.sh
+++
@@ -1,8 +0,0 @@
-cd ~
-wget https://polybox.ethz.ch/index.php/s/C4YaQ1zsmWXctk2/download
-mv download SSSP_modified.tar.gz
-tar xvf SSSP_modified.tar.gz
-rm SSSP_modified.tar.gz
-
-verdi data upf uploadfamily SSSP_modified SSSP_modified "SSSP modified"
-
